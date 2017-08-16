68224842af0caefa1bdc224a8520ee727059cfdb
akender <arno.kender@gmail.com>
2017-08-16 23:11:02 +0300

    Create weathermap.footer.inc.php

---
+++ lib/weathermap.footer.inc.php
@@ -0,0 +1,16 @@
+        <div class="navbar navbar-fixed-bottom">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="oicon-bar"></span>
+                        <span class="oicon-bar"></span>
+                        <span class="oicon-bar"></span>
+                    </a>
+                    <div class="nav-collapse">
+                        <ul class="nav">
+                            <li class="dropdown">PHP Weathermap <?php print $GLOBALS['config_weathermap_version']; ?></li>
+                        </ul>
+                    </div>
+                </div><!-- container -->
+            </div><!-- navbar-inner -->
+        </div><!-- navbar-fixed-bottom -->
