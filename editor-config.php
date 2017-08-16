5dc7ebb0f9f21eee774bcf444f3b1e7d221ceb2e
akender <arno.kender@gmail.com>
2017-08-16 23:07:08 +0300

    Create editor-config.php

---
+++ editor-config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config_weathermap_groups = array(
+    "gr1"=>"Group1", 
+    "gr2"=>"Group2");
+
+$config_weathermap_observiumbase    = 'https://yoursite.com:443/';
+$config_weathermap_mapdir           = 'configs';
+$config_weathermap_version          = '0.97c';
+
+?>
